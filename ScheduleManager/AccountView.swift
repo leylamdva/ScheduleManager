c5ed2e910b1d217e5985791d033854459c78ebc0
leylamdva <l.mammadova@hotmail.com>
2022-12-21 22:41:32 +0300

    Account Page added

--- ScheduleManager/AccountView.swift
+++ ScheduleManager/AccountView.swift
@@ -9,12 +9,82 @@ import SwiftUI
 
 struct AccountView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack(alignment: .leading){
+                // Account information
+                HStack{
+                    Image(systemName: "person.circle.fill")
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                    Spacer()
+                    Text("ExampleEmail/@gmail.com")
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                        .font(.title3)
+                }
+                .padding(.horizontal, 30)
+                Divider()
+                    .frame(minHeight: 1)
+                    .background(Color.gray)
+                    .padding(.horizontal)
+                
+                // Location
+                HStack {
+                    NavigationLink(destination: Text("Location View"), label: {
+                        Text("Location")
+                            .font(.title3)
+                    })
+                        .buttonStyle(PlainButtonStyle())
+                    Spacer()
+                    Image(systemName: "chevron.right")
+                }
+                .padding(20)
+                
+                // TODO: Separate the modifier and stacks
+                // Clear all data
+                Button{
+                    // TODO: clear data implementation
+                }label: {
+                    Text("Clear all data")
+                        .underline()
+                        .foregroundColor(.red)
+                }
+                .padding(.horizontal, 20)
+                .padding(.vertical, 5)
+                
+                // Reset Password
+                Button{
+                    // TODO: reset password implementation
+                }label: {
+                    Text("Reset Password")
+                        .underline()
+                        .foregroundColor(.red)
+                }
+                .padding(.horizontal, 20)
+                .padding(.vertical, 5)
+                
+                Spacer()
+                
+                // Log Out
+                Button{
+                    // TODO: logout implementation
+                }label: {
+                    Text("Log Out")
+                        .foregroundColor(.blue)
+                }
+                .padding(.vertical, 20)
+                // TODO: fix alignment
+                .padding(.horizontal, 180)
+                
+            } //VStack
+            .preferredColorScheme(.dark)
+        }
     }
 }
 
 struct AccountView_Previews: PreviewProvider {
     static var previews: some View {
         AccountView()
+            .preferredColorScheme(.dark)
     }
 }
